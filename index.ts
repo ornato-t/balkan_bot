78b3e041d95e542ede2944fee8bc5046644afbad
ornato-t <tommy.ornato@gmail.com>
2023-04-14 10:15:40 +0200

    Replying and pinning comment under every post

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { CommentStream } from 'snoostorm';
+import { CommentStream, SubmissionStream } from 'snoostorm';
 import Snoowrap from 'snoowrap';
 
 import noFlair from './modules/unflaired.js';
@@ -20,11 +20,15 @@ const comments = new CommentStream(r, {
     subreddit: 'balkans_irl',
 });
 
+//Configure a comment stream
+const posts = new SubmissionStream(r, {
+    subreddit: 'balkans_irl',
+});
+
 const blacklist = ['SaveVideo', 'eazeaze']; //Fill this array with the names of the accounts that should be ignored 
 
 run();
 
-//Main function
 async function run() {
     console.log('Starting up...');
     if (c.DEBUG) console.log('Warning, DEBUG mode is ON');
@@ -36,14 +40,42 @@ async function run() {
         try {
             const flair = comment.author_flair_template_id;
 
+            //If the user is unflaired insult them and tell them to flair up
             if (flair == null) {
                 unflaired(comment);
             }
 
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (e: any) { console.log(e.toString()) }
+        } catch (e) { console.log(e) }
+    });
+
+    //Function executed for every post
+    posts.on('item', async post => {
+        const modComment = getImageBoard(); //Comment to be pinned
+
+        const id = await reply(post, modComment);   //Reply to the comment and save the resulting ID
+        if (id !== null) {
+            const comment = r.getComment(id);
+            pinComment(comment);
+        }
     });
 }
+
+//Replies to a comment or a post, only if DEBUG mode is off. Return the id of the reply or null
+async function reply(target: Snoowrap.Comment | Snoowrap.Submission, msg: string) {
+    try {
+        if (!c.DEBUG) {
+            return target.reply(msg).then(res => res.id);
+        } else {
+            console.log(`DEBUG:\n${msg}`);
+            return null;
+        }
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+}
+
+/*  UNFLAIRED FUNCTIONS   */
 
 //Sends a random message reminding users to flair up. Only answers in a percentage of cases
 async function unflaired(comment: Snoowrap.Comment) {
@@ -68,14 +100,20 @@ function percentage(n: number) {
     else return false
 }
 
-//Replies to a message, only if DEBUG mode is off
-async function reply(comment: Snoowrap.Comment, msg: string) {
+
+/*  MOD MESSAGE FUNCTIONS   */
+
+//Return the image board string
+function getImageBoard() {
+    const str = "Are you having fun browsing the sub? Well there is more stuff coming down the line including the official balkans_irl imageboard:\n\nhttps://www.reddit.com/r/balkans_irl/comments/10sl5hr/announcing_the_balkans_irl_imageboard/   \n\n*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/balkans_irl) if you have any questions or concerns.*";
+
+    return str;
+}
+
+//Distinguish a comment as moderator and pin it
+function pinComment(comment: Snoowrap.Comment) {
     try {
-        if (!c.DEBUG) {
-            comment.reply(msg);
-        } else {
-            console.log(`DEBUG:\n${msg}`);
-        }
+        comment.distinguish({ status: true, sticky: true });
     } catch (e) {
         console.log(e);
     }
